e2ced963bee600952e702a0dfcef6edc4653c2cb
Maksim Iakunin <yakuninm+github@gmail.com>
2024-05-27 02:59:49 +0300

    Update run.sh

--- amazon-s3-backup/run.sh
+++ amazon-s3-backup/run.sh
@@ -9,6 +9,7 @@ bashio::log.info "Starting Amazon S3 Backup..."
 bucket_name="$(bashio::config 'bucket_name')"
 bucket_region="$(bashio::config 'bucket_region')"
 monitor_path="$(bashio::config 'monitor_path')"
+endpoint_url="$(bashio::config 'endpoint_url')"
 
 export AWS_ACCESS_KEY_ID="$(bashio::config 'aws_access_key')"
 export AWS_SECRET_ACCESS_KEY="$(bashio::config 'aws_secret_access_key')"
@@ -16,6 +17,6 @@ export AWS_REGION="$bucket_region"
 
 bashio::log.debug "Using AWS CLI version: '$(aws --version)'"
 bashio::log.debug "Command: 'aws s3 sync $monitor_path s3://$bucket_name/ --no-progress --region $bucket_region'"
-aws s3 sync $monitor_path s3://"$bucket_name"/ --no-progress --region "$bucket_region"
+aws s3 sync $monitor_path s3://"$bucket_name"/ --no-progress --region "$bucket_region" --endpoint-url "$endpoint_url"
 
 bashio::log.info "Finished Amazon S3 Backup."
