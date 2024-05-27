a5d0354385c6872eb4b4a5a7831d8e228b043147
Maksim Iakunin <yakuninm+github@gmail.com>
2024-05-27 13:47:38 +0300

    version 1.3.3

--- amazon-s3-backup/run.sh
+++ amazon-s3-backup/run.sh
@@ -4,19 +4,44 @@
 # ==============================================================================
 #bashio::log.level "debug"
 
+function abspath() {
+    # generate absolute path from relative path
+    # $1     : relative filename
+    # return : absolute path
+    if [ -d "$1" ]; then
+        # dir
+        (cd "$1"; pwd)
+    elif [ -f "$1" ]; then
+        # file
+        if [[ $1 = /* ]]; then
+            echo "$1"
+        elif [[ $1 == */* ]]; then
+            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
+        else
+            echo "$(pwd)/$1"
+        fi
+    fi
+}
+
 bashio::log.info "Starting Amazon S3 Backup..."
 
+backup_path="$(bashio::config 'backup_path')"
 bucket_name="$(bashio::config 'bucket_name')"
 bucket_region="$(bashio::config 'bucket_region')"
-monitor_path="$(bashio::config 'monitor_path')"
 endpoint_url="$(bashio::config 'endpoint_url')"
+remote_path="s3://$bucket_name$(abspath "$backup_path")"
 
 export AWS_ACCESS_KEY_ID="$(bashio::config 'aws_access_key')"
 export AWS_SECRET_ACCESS_KEY="$(bashio::config 'aws_secret_access_key')"
-export AWS_REGION="$bucket_region"
+#export AWS_REGION="$bucket_region"
+
 
 bashio::log.debug "Using AWS CLI version: '$(aws --version)'"
-bashio::log.debug "Command: 'aws s3 sync $monitor_path s3://$bucket_name/ --no-progress --region $bucket_region --endpoint-url $endpoint_url'"
-aws s3 sync $monitor_path s3://"$bucket_name"/ --no-progress --region "$bucket_region" --endpoint-url "$endpoint_url"
+#bashio::log.debug "Command: 'aws s3 sync $backup_path s3://$bucket_name/ --no-progress --region $bucket_region --endpoint-url $endpoint_url'"
+set -x
+aws s3 sync "$backup_path" "$remote_path" \
+  --delete \
+  --region "$bucket_region" \
+  --endpoint-url "$endpoint_url"
 
 bashio::log.info "Finished Amazon S3 Backup."
